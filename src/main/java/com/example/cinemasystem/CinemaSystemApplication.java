f132e5877634ed9ab949352682c6c55e9738b7b5
Kuba Kosiński <kosinski.jakub01@gmail.com>
2025-11-29 20:17:06 +0100

    add postgres config

--- src/main/java/com/example/cinemasystem/CinemaSystemApplication.java
+++ src/main/java/com/example/cinemasystem/CinemaSystemApplication.java
@@ -1,13 +1,27 @@
 package com.example.cinemasystem;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class CinemaSystemApplication {
+@Log
+public class CinemaSystemApplication implements CommandLineRunner {
+    private final DataSource dataSource;
 
+    public CinemaSystemApplication(DataSource dataSource) { this.dataSource = dataSource; }
     public static void main(String[] args) {
         SpringApplication.run(CinemaSystemApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Datasource " + dataSource.toString());
+        final JdbcTemplate template = new JdbcTemplate(dataSource);
+        template.execute("select 1");
+    }
 }
