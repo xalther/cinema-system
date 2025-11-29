42c5deddb2666ce53685a5f9d3bda1349f7f3f87
Kuba Kosiński <kosinski.jakub01@gmail.com>
2025-11-29 19:39:42 +0100

    first commit

---
+++ src/main/java/com/example/cinemasystem/CinemaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaSystemApplication.class, args);
+    }
+
+}
